f3d1da4db106232e16b0f583db390fdb5d1db07c
Ubuntu <ubuntu@ip-172-31-24-36.us-west-2.compute.internal>
2019-04-07 06:17:05 +0000

    room_state

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask
 from models.Song import Song
+from models.State import room_state
 
 app = Flask(__name__)
 
+states = {}
+
+@app.route("/create/<room>")
+def create_room(room):
+    songs = []
+    for x in range(10):
+        songs.append(Song(title="Sweet Child O' Mine", album="Appetite For Destruction",
+            uri="a url", user="rasimon"))
+    states[room] = room_state(room_code=room, queue=songs,
+            playback_status="playing", members='rasimon')
+    return states[room]
 @app.route("/join/<room>")
 def join_room(room):
-    return str(Song(title="A Title", album="An album", uri="URI", user="user"))
+    return str(states[room])
 @app.route('/create')
 
 @app.route("/")
 def hello():
     return "Hello World"
+app.run(host="0.0.0.0")
--- models/State.py
+++ models/State.py
@@ -1,7 +1,19 @@
-class State:
+import json
+class room_state(object):
     def __init__(self, **kwargs):
-        self.queue = kwargs['queue']
-        self.current_song = kwargs['current_song']
-        self.members = kwargs['members']
-        self.playback_status = kwargs['playback_status']
-        self.room_code = kwargs['room_code']
+        #self.queue = kwargs['queue']
+        #self.members = kwargs['members']
+        #kself.playback_status = kwargs['playback_status']
+        #self.room_code = kwargs['room_code']
+        self.state = kwargs
+        self.state['current_song'] = self.state['queue'][0]
+
+    def __str__(self):
+        return json.dumps(self.state)
+    def add_song(self, song):
+        self.state['queue'].append(song)
+
+    def add_member(self, member):
+        self.state['members'].append(member)
+
+
